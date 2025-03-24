93a4c3441e781b8c1ba012fa0688aa36dfd3d0b2
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-03-24 10:17:35 -0600

    Se agregaron los [Authorize] en los controllers.

--- EGNS20250324.AppWebMVC/Controllers/UsersController.cs
+++ EGNS20250324.AppWebMVC/Controllers/UsersController.cs
@@ -15,6 +15,7 @@ using System.Security.Claims;
 
 namespace EGNS20250324.AppWebMVC.Controllers
 {
+    [Authorize(Roles = "ADMINISTRADOR")]
     public class UsersController : Controller
     {
         private readonly Test20250324DbContext _context;
